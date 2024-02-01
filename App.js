e4b194e377c78bf7d20dc6331badf991d735acaa
EsoraKyugo <tom.7trouillet@gmail.com>
2024-02-01 14:23:11 +0100

    mise en forme ~

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   Image,
-  ScrollView,
   Dimensions,
   ImageBackground,
-  FlatList,
 } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function Homepage() {
   const [cookRand, setCookRand] = useState(null);
   const allIngredients = cookRand && cookRand.drinks
-  ? Array.from({ length: 15 }, (_, index) => cookRand.drinks[0][`strIngredient${index + 1}`]).filter(Boolean)
-  : [];
-
+    ? Array.from({ length: 15 }, (_, index) => cookRand.drinks[0][`strIngredient${index + 1}`]).filter(Boolean)
+    : [];
 
   function cooktailRand() {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
       .then((resp) => resp.json())
       .then((cookRand) => {
         setCookRand(cookRand);
@@ -36,8 +32,7 @@ export default function Homepage() {
   }, []);
 
   const image = {
-    uri:
-      'https://previews.123rf.com/images/yelenayemchuk/yelenayemchuk1704/yelenayemchuk170400393/77093691-mojito-de-cocktail-rafra%C3%AEchissant-menthe-citron-vert-avec-rhum-et-glace-en-verre-sur-fond-noir.jpg',
+    uri: 'https://static.vecteezy.com/ti/photos-gratuite/p2/16655414-degrade-de-vert-et-de-jaune-gratuit-photo.jpg',
   };
 
   return (
@@ -51,21 +46,23 @@ export default function Homepage() {
         {cookRand && cookRand.drinks && (
           <View style={styles.infBlock}>
             <View style={styles.name}>
-              <Text>{cookRand.drinks[0].strDrink}</Text>
+              <Text style={styles.drinkName}>{cookRand.drinks[0].strDrink}</Text>
             </View>
             <View style={styles.imageText}>
               <Image
                 source={{ uri: cookRand.drinks[0].strDrinkThumb }}
                 style={styles.image}
               />
-              {/* changer pour avoir une liste plutot que des text */}
+              {/* Changed to use a flat list for better structure */}
               <Text style={styles.desc}>
-                Ingredient:{' '}
+                Ingredients:{'\n'}
                 {allIngredients.map((ingredient, index) => (
-                  <FlatList key={index}>{ingredient}\n</FlatList>
+                  <Text key={index} style={styles.ingredient}>
+                    {ingredient}
+                    {'\n'}
+                  </Text>
                 ))}
               </Text>
-              {/* changer pour avoir une liste plutot que des text */}
             </View>
           </View>
         )}
@@ -88,23 +85,40 @@ const styles = StyleSheet.create({
 
   infBlock: {
     flex: 1,
-    color: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
+  name: {
+    marginBottom: 20,
+  },
+
+  drinkName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
   imageText: {
-    height: 200,
-    width: 200,
+    height: 500,
+    width: 250,
+    alignItems: 'center',
   },
 
   image: {
     flex: 1,
-    width: windowWidth,
-    height: windowHeight,
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
   },
 
   desc: {
+    textAlign: 'center',
+    color: 'black',
+    marginTop: 20,
+  },
 
+  ingredient: {
+    color: 'black',
   },
 });
